3a8eb8c686c7dffad348d57cb6a06eac30fe8619
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2026-04-02 22:17:51 +0300

    install mongoose and set mongo connection in logging

--- apps/logging/src/logging.module.ts
+++ apps/logging/src/logging.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LoggingController } from './logging.controller';
 import { LoggingService } from './logging.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/27017/uber_logging_db'),
+  ],
   controllers: [LoggingController],
   providers: [LoggingService],
 })
