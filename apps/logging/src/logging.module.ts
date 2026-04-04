26b171cd4e195142543abbd0a4b2e29ffe1a82a5
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2026-04-04 22:57:22 +0300

    adding rider-coordinates mudule in logging app

--- apps/logging/src/logging.module.ts
+++ apps/logging/src/logging.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { LoggingController } from './logging.controller';
 import { LoggingService } from './logging.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RiderCoordinatesModule } from './rider-coordinates/rider-coordinates.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/27017/uber_logging_db'),
+    RiderCoordinatesModule,
   ],
   controllers: [LoggingController],
   providers: [LoggingService],
